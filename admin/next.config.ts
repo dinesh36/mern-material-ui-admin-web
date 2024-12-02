b4af33dda0d47375f896bb7fb322965fc244615c
dinesh <ddinesh.36@gmail.com>
2024-12-02 10:06:11 +0530

    Integrate Babel for TS/JSX in shared components

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.[jt]sx?$/, // Match JavaScript/TypeScript files
+      include: [path.resolve(__dirname, '../common')], // Include the common folder
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'], // Use Next.js Babel presets
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
