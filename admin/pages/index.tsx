670026bebbd5e673ded07aa695d1db767efbaeda
dinesh <ddinesh.36@gmail.com>
2024-12-23 10:50:56 +0530

    Update dependencies and introduce local UI shared module

--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import { Button } from '@mern/ui-shared';
 
 export default function Home() {
   return (
       <Stack spacing={2} direction="row">
-        <Button variant="outlined" color="secondary">
-          Admin Home page
-        </Button>
+          <h1>This is admin page</h1>
+          <Button />
       </Stack>
   );
 }
