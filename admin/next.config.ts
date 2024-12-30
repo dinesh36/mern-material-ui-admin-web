8005a0122a449b0a53f1bf266bc09430aacac775
dinesh <ddinesh.36@gmail.com>
2024-12-30 19:19:54 +0530

    Update dependencies and introduce local UI shared module (#6)

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,8 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.[jt]sx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react",
+            "@babel/preset-typescript",
+          ],
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
