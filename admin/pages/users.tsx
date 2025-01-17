c27b395c9ffd26c46441ab0b5bf80523d59e121e
dinesh <ddinesh.36@gmail.com>
2025-01-17 12:17:00 +0530

    setup ag-grid-table

--- admin/pages/users.tsx
+++ admin/pages/users.tsx
@@ -1,68 +1,65 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/module/redux/store";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Avatar,
-  Typography,
-} from "@mui/material";
-import { User } from "@/models/auth.type";
-import { getUserDetails } from "@/module/services/auth-services";
+import React, {StrictMode, useEffect, useMemo, useState} from "react";
 
-const UserTable: React.FC = () => {
-  const user = useSelector((state: RootState) => state.auth.user);
-  const [userDetails, setUserDetails] = React.useState<User[]>([]);
+import type { ColDef, RowSelectionOptions } from "ag-grid-community";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import {User} from "@/models/auth.type";
+import {getUserDetails} from "@/module/services/auth-services";
 
-  const fetchUserDetails = async () => {
-    try {
-      const fetchedUsers = await getUserDetails();
-      setUserDetails(fetchedUsers);
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-    }
-  };
+ModuleRegistry.registerModules([AllCommunityModule]);
 
-  React.useEffect(() => {
-    fetchUserDetails();
-  }, []);
 
-  if (!user) return null;
-
-  return (
-      <TableContainer>
-        <Table aria-label="user table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Profile Image</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {userDetails.map((userDetail) => (
-                <TableRow key={userDetail.id}>
-                  <TableCell>
-                    <Avatar
-                        sx={{ width: 32, height: 32 }}
-                        src={userDetail.profileImage || "default_image_path.jpg"}
-                        alt={userDetail.name || "User Avatar"}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body2">{userDetail.name}</Typography>
-                  </TableCell>
-                  <TableCell>{userDetail.email}</TableCell>
-                </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-  );
+const rowSelection: RowSelectionOptions = {
+    mode: "multiRow",
+    headerCheckbox: false,
 };
 
-export default UserTable;
+const GridExample = () => {
+    const [userDetails, setUserDetails] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchUserDetails = async () => {
+        setLoading(true);
+        try {
+            const fetchedUsers = await getUserDetails();
+            setUserDetails(fetchedUsers);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUserDetails();
+    }, []);
+
+
+    const [columnDefs, setColumnDefs] = useState<ColDef[]>([
+        { field: "name", editable: true, cellEditor: "agSelectCellEditor" },
+        { field: "email" },
+        { field: "profileImage" },
+    ]);
+
+    const defaultColDef = useMemo(() => {
+        return {
+            filter: "agTextColumnFilter",
+            floatingFilter: true,
+        };
+    }, []);
+
+    return (
+        <div style={{ height: 500 }}>
+            <AgGridReact
+                rowData={userDetails}
+                columnDefs={columnDefs}
+                defaultColDef={defaultColDef}
+                rowSelection={rowSelection}
+                pagination={true}
+                paginationPageSize={10}
+                paginationPageSizeSelector={[10, 25, 50]}
+            />
+        </div>
+    );
+};
+
+export default GridExample;
